cc722b78341873fdc87c6da954c2eb4dc38a7e85
Hassan <hassancreative9@gmail.com>
2019-11-15 02:58:28 +0500

    Add splash screen

---
+++ src/components/screen.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react' 
+
+class Screen extends Component{
+   
+     
+ 
+
+    render(){
+        return(
+            <div>
+                 
+                 lasdasl                
+            </div>
+        )
+    }
+}
+
+export default Screen;
